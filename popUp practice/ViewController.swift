345a6b26229b458a994109b902a807fbc21b07d5
Valerie Kwan <valvalkwan@gmail.com>
2022-07-20 11:25:28 -0700

    Initial Commit

---
+++ popUp practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  popUp practice
+//
+//  Created by Scholar on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
